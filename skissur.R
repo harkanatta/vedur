9b25b5893207e4f24d7e7d605462997bec55b854
Valtýr Sigurðsson <valtyr@biopol.is>
2021-01-06 13:09:33 +0000

    Komið þetta fína echarter dagatal í skissur.R ásamt veðurgögnum frá 01-01-2011

--- skissur.R
+++ skissur.R
@@ -1,51 +1,213 @@
+
+rass <- read.csv('Skagastr&ouml;nd_01012020-31122020.txt',header = T,sep = "\t")
+
+
 library(echarts4r)
 library(magrittr)
-library(lubridate)
-dates <- seq.Date(as.Date("2017-01-01"), as.Date("2018-12-31"), by = "day")
-values <- rnorm(length(dates), 20, 6)
-year <- data.frame(date = dates, values = values)
-year %>% 
-  e_charts(date) %>% 
-  e_calendar(range = "2018",
-             top="60", 
-             left = 150, 
-             dayLabel = list(
-               firstDay=2, 
-               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
-             monthLabel = list(
-               nameMap = month(1:12, label = TRUE, abbr = T))) %>% 
-  e_heatmap(values, coord_system = "calendar") %>% 
-  e_visual_map(max = 30) %>% 
-  e_title("Calendar", "Heatmap")
-
-
-
-
-
 library(plyr)
+library(purrr)
 
-rass <- vedur %>% 
-  select(date="dags2",values="Lofthiti..degC.","dagur") %>% 
-  ddply(rass,"dagur",summarize,values=mean("values"))
+file <- "C:/Users/valty/Documents/vinna/github/vedur/skjol"
+setwd(file)
+files <- list.files(pattern="*.txt")
+vedur <- files %>% map_dfr(read.csv2, sep="\t") 
 
-vedur %>% 
-  select(date="dags2",values="Lofthiti..degC.","dagur") %>%
-  ddply("dagur",summarize,date=mean("values")) %>% 
-  e_charts(date) %>% 
-  e_calendar(range = "2018",
-             top="60", 
-             left = 150, 
+vedur$dags <- strptime(vedur$Timabil, "%H:%M %d.%m.%Y")
+#vedur$dags2 <- strptime(vedur$Timabil, "%d/%m/%Y %H:%M")
+vedur$dagsw <- ifelse(is.na(as.Date(as.character(vedur$Timabil),"%H:%M %d.%m.%Y")),as.character.Date(vedur$dags2),as.character.Date(vedur$dags))
+vedur$dags <- as.POSIXct(vedur$dagsw,tz="GMT")
+vedur <- vedur[order(vedur$dags,decreasing=F),]
+vedur$dags2 <- as.Date(vedur$dags,format="%d.%m.%Y")
+vedur$year <- format(vedur$dags,"%Y")
+vedur <- vedur[!vedur$year==2021,]
+
+g <- vedur$year
+l <- split(vedur, g)
+l <- lapply(l, function(x) mean(x$Lofthiti..degC.))
+
+
+
+vedur %>% ddply(.(dags2),summarize,'°C'=round(mean(Lofthiti..degC.),1)) %>% 
+  dplyr::mutate(year = format(dags2, "%Y")) %>% # get year from date
+  group_by(year) %>%
+  e_charts(dags2) %>% 
+  e_calendar(range = "2011",top="50", cellSize="10",
+             left = "5%", 
+             width = "40%",
              dayLabel = list(
                firstDay=2, 
                nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
              monthLabel = list(
                nameMap = month(1:12, label = TRUE, abbr = T))) %>% 
-  e_heatmap(values, coord_system = "calendar") %>% 
-  e_visual_map(max = 30) %>% 
-  e_title("Calendar", "Heatmap")
+  e_calendar(range = "2012",top="140", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2013",top="230", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2014",top="320", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2015",top="410", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2016",top="50", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>% 
+  e_calendar(range = "2017",top="140", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2018",top="230", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2019",top="320", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2020",top="410", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_heatmap(`°C`, coord_system = "calendar") %>% 
+  e_visual_map(max = 16, min = -11) %>% 
+  e_title("Hitastig", "Skagaströnd") %>% 
+  e_tooltip("item")
 
 
 
+
+
+vedur %>% ddply(.(dags2),summarize,'°C'=round(max(Hvida..m.s.),1)) %>% 
+  dplyr::mutate(year = format(dags2, "%Y")) %>% # get year from date
+  group_by(year) %>%
+  e_charts(dags2) %>% 
+  e_calendar(range = "2011",top="50", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>% 
+  e_calendar(range = "2012",top="140", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2013",top="230", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2014",top="320", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2015",top="410", cellSize="10",
+             left = "5%", 
+             width = "40%",
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2016",top="50", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>% 
+  e_calendar(range = "2017",top="140", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2018",top="230", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2019",top="320", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_calendar(range = "2020",top="410", cellSize="10",
+             right = "5%", 
+             width = "40%", 
+             dayLabel = list(
+               firstDay=2, 
+               nameMap = c('lau', 'sun', 'mán', 'þri', 'mið', 'fim', 'fös')),
+             monthLabel = list(
+               nameMap = month(1:12, label = TRUE, abbr = T))) %>%
+  e_heatmap(`°C`, coord_system = "calendar") %>% 
+  e_visual_map(max = 50, min = 0) %>% 
+  e_title("Hitastig", "Skagaströnd") %>% 
+  e_tooltip("item")
 
 
 
